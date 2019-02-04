c3556a274c95e6a73f2e481d258b47d791092065
ruwanditha <piumal512@gmail.com>
2019-02-04 15:00:43 +0530

    FirstCommit

---
+++ demogithub/src/main/java/com/hellovscodewgithub/demogithub/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.hellovscodewgithub.demogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	@GetMapping(value="/hello")
+	public String getMethodName() {
+		return "Hello GitHub with Spring Boot";
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
